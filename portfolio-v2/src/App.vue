fa52cc75d289d86e5d8b03f463dc968a9f7c000f
Bill Kariri <billkariri@gmail.com>
2020-03-02 19:11:05 +0300

    create a basic vuejs app

---
+++ portfolio-v2/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Portfolio/>
+  </div>
+</template>
+
+<script>
+import Portfolio from './components/Portfolio.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Portfolio
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ portfolio-v2/src/components/Portfolio.vue
@@ -0,0 +1,569 @@
+<template>
+  <div>
+    <div class="hero">
+    <h1 class="ml1">
+      <span class="text-wrapper">
+        <span class="line line1"></span>
+        <span class="letters">Bill Kariri</span>
+        <span class="line line2"></span>
+      </span>
+    </h1>
+
+    <h4 class="ml16">Software Developer</h4>
+
+    <div class="more-button">
+      <p>Check out my work!</p>
+      <i class="fal fa-chevron-down"></i>
+    </div>
+  </div>
+
+  <div class="section light">
+    <h2 id="projects" class="subheading">
+      Open Source Projects
+      <span class="line"></span>
+    </h2>
+    <div class="center-projects">
+      <div class="project-card">
+        <div class="heading">
+          <h6>Automart</h6>
+        </div>
+        <div class="body">
+          <p>An online car marketplace API</p>
+        </div>
+        <div class="footer">
+          <div>
+            <a href="https://automart-api.herokuapp.com/docs/" target="_blank" rel="noopener noreferrer">
+              <i class="fa fa-eye"></i> View
+            </a>
+          </div>
+          <a href="https://github.com/BILLthebuilder/automart" target="_blank" rel="noopener">
+            <i class="fab fa-github"></i> Repository
+          </a>
+        </div>
+      </div>
+      <div class="project-card">
+        <div class="heading">
+          <h6>Feed-reader</h6>
+        </div>
+        <div class="body">
+          <p>An RSS feeds application that utilizes the Jasmine testing framework for unit tests</p>
+        </div>
+        <div class="footer">
+          <div>
+            <a href="https://billthebuilder.github.io/feed-reader" target="_blank" rel="noopener noreferrer">
+              <i class="fa fa-eye"></i> View
+            </a>
+          </div>
+          <a href="https://github.com/BILLthebuilder/feed-reader" target="_blank" rel="noopener">
+            <i class="fab fa-github"></i> Repository
+          </a>
+        </div>
+      </div>
+      <div class="project-card">
+        <div class="heading">
+          <h6>Nyumbani</h6>
+        </div>
+        <div class="body">
+          <p>A real estate web application </p>
+        </div>
+        <div class="footer">
+          <div>
+            <a href="https://billthebuilder.github.io/nyumbani/" target="_blank" rel="noopener noreferrer">
+              <i class="fa fa-eye"></i> View
+            </a>
+          </div>
+          <a href="https://github.com/BILLthebuilder/nyumbani" target="_blank" rel="noopener">
+            <i class="fab fa-github"></i> Repository
+          </a>
+        </div>
+      </div>
+      <div class="project-card">
+        <div class="heading">
+          <h6>Mawazo</h6>
+        </div>
+        <div class="body">
+          <p>A simple Blogging API</p>
+        </div>
+        <div class="footer">
+          <div>
+            <a href="https://auth-api-mongo.herokuapp.com/users/all" target="_blank" rel="noopener noreferrer">
+              <i class="fa fa-eye"></i> View
+            </a>
+          </div>
+          <a href="https://github.com/BILLthebuilder/mawazo" target="_blank" rel="noopener">
+            <i class="fab fa-github"></i> Repository
+          </a>
+        </div>
+      </div>
+      <div class="project-card">
+        <div class="heading">
+          <h6>Usajili</h6>
+        </div>
+        <div class="body">
+          <p>A user registration system template</p>
+        </div>
+        <div class="footer">
+          <div>
+            <a href="https://app-usajili.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+              <i class="fa fa-eye"></i> View
+            </a>  
+          </div>
+          <a href="https://github.com/BILLthebuilder/usajili" target="_blank" rel="noopener">
+            <i class="fab fa-github"></i> Repository
+          </a>
+        </div>
+      </div>
+      <div class="project-card">
+        <div class="heading">
+          <h6>Restaurant-reviews</h6>
+        </div>
+        <div class="body">
+          <p>An offline first and responsive web application for reviewing restaurants built with pure ES6 Javascript</p>
+        </div>
+        <div class="footer">
+          <div>
+            <a href="https://res-review.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+              <i class="fa fa-eye"></i> View
+            </a>
+          </div>
+          <a href="https://github.com/BILLthebuilder/restaurant-reviews" target="_blank" rel="noopener">
+            <i class="fab fa-github"></i> Repository
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section light">
+    <h2 class="subheading">
+      Professional Skills
+      <span class="line"></span>
+    </h2>
+    <div class="skills">
+      <div class="skills-section">
+        <h3>Languages</h3>
+        <ul>
+          <li>JavaScript</li>
+          <li>NodeJS</li>
+          <li>SQL</li>
+          <li>Python</li>
+        </ul>
+      </div>
+      <div class="skills-section">
+        <h3>Web Frameworks</h3>
+        <ul>
+          <li>React</li>
+          <li>Vue</li>
+          <li>Flask</li>
+          <li>Django</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="section dark">
+    <div class="social-links">
+      <a href="https://github.com/BILLthebuilder" target="_blank" rel="noopener">
+        <i class="fab fa-github"></i>
+      </a>
+      <a href="https://linkedin.com/in/bkariri" target="_blank" rel="noopener">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="https://twitter.com/billkariri" target="_blank" rel="noopener">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="#">
+        <i class="fab fa-stack-overflow"></i>
+      </a>
+      <a href="#">
+        <i class="fab fa-medium"></i>
+      </a>
+      <a href="#">
+        <svg xmlns="http://www.w3.org/2000/svg" version="1" width="544" height="546.667" viewBox="0 0 408 410">
+          <path
+            d="M10.5 2.5C5.6 4.2 2.4 7.8 1 12.9.3 15.6.1 77.2.2 207.1.5 396.6.5 397.5 2.5 401c1.2 1.9 3.6 4.3 5.5 5.5 3.5 2 4.2 2 196 2s192.5 0 196-2c1.9-1.2 4.3-3.6 5.5-5.5 2-3.5 2-4.2 2-196s0-192.5-2-196c-1.2-1.9-3.6-4.3-5.5-5.5-3.5-2-4.4-2-194.5-2.2-162.9-.2-191.6 0-195 1.2zm96.8 124c9 1.9 15.5 5.6 22.2 12.6 6.1 6.4 9.9 13.7 11.5 21.9 1.3 6.9 1.3 81.1 0 88.1-3.4 18.2-18.9 32.4-38.1 34.9-4 .5-17.4 1-29.6 1H51V125h24.8c17.6 0 26.6.4 31.5 1.5zm130.7 13V154h-51v36h31v29h-31l.2 18.2.3 18.3 25.3.3 25.2.2v29h-29.5c-32.8 0-37.9-.6-43.1-5.2-7.3-6.4-6.9-2.3-7.2-72.6-.2-43.8.1-64.5.8-67.3 1.4-5.2 7.3-11.6 12.5-13.4 3.1-1.1 10.9-1.4 35.3-1.5H238v14.5zm48.9-12.3c.4 1.3 6.8 25.6 14.2 54 7.4 28.5 13.6 51.2 13.8 50.5.3-.7 6.6-24.8 14.1-53.7l13.7-52.5 15.6-.3c11.8-.2 15.7.1 15.7 1s-34.6 132.4-36.6 139c-1.4 4.6-8.9 14-13.1 16.5-5.3 3-13.1 3.5-17.9 1-4.3-2.2-10-8.8-12.9-15-1.8-3.8-31.2-112-38-140l-.7-2.7h15.6c15.3 0 15.7 0 16.5 2.2z" />
+          <path
+            d="M80 205v51.2l10.3-.4c8.9-.3 10.8-.7 14.8-3 7.8-4.6 7.9-4.9 7.9-48.4 0-42.4 0-42.4-7.3-47.2-3.6-2.4-5.1-2.7-14.9-3.1l-10.8-.3V205z" />
+        </svg>
+      </a>
+    </div>
+    <p class="copyright">Copyright &copy; {{new Date().getFullYear()}} - Bill Kariri</p>
+  </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Portfolio'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+html,
+body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  font-family: 'PT Mono', monospace;
+}
+
+.hero {
+  height: 100%;
+  width: 100%;
+  background-color: #213377;
+  color: #dfe6e9;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
+
+.ml1 {
+  font-weight: 900;
+  font-size: 3.5em;
+}
+
+.ml1 .letter {
+  display: inline-block;
+  line-height: 1em;
+  opacity: 0;
+}
+
+.ml1 .text-wrapper {
+  position: relative;
+  display: inline-block;
+  padding-top: 0.1em;
+  padding-right: 0.05em;
+  padding-bottom: 0.15em;
+}
+
+.ml1 .line {
+  opacity: 0;
+  position: absolute;
+  left: 0;
+  height: 2px;
+  width: 100%;
+  background-color: #fff;
+  -webkit-transform-origin: 0 0;
+  transform-origin: 0 0;
+}
+
+.ml1 .line1 {
+  top: 0;
+}
+.ml1 .line2 {
+  bottom: 0;
+}
+
+.ml16 {
+  /* color: #402d2d; */
+  padding: 40px 0;
+  font-weight: 800;
+  font-size: 2em;
+  text-transform: uppercase;
+  letter-spacing: 0.3em;
+  overflow: hidden;
+}
+
+.ml16 .letter {
+  display: inline-block;
+  line-height: 1em;
+  opacity: 0;
+}
+
+.section {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-box-pack: start;
+  -ms-flex-pack: start;
+  justify-content: flex-start;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  padding: 40px;
+}
+.section.light {
+  background-color: #dfe6e9;
+  color: rgba(0, 0, 0, 0.8);
+}
+.section.dark {
+  background-color: #213377;
+  color: #dfe6e9;
+}
+
+.subheading {
+  position: relative;
+  line-height: 45px;
+}
+.subheading .line {
+  position: absolute;
+  left: 0;
+  height: 2px;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  -webkit-transform-origin: 0 0;
+  transform-origin: 0 0;
+  bottom: 0;
+}
+
+.center-projects {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  width: 1140px;
+}
+
+.more-button {
+  text-align: center;
+  position: absolute;
+  bottom: 15px;
+  cursor: pointer;
+  opacity: 0;
+}
+
+.more-button > .fal {
+  font-size: 30px;
+}
+
+.more-button > p {
+  margin: 0 0 10px 0;
+}
+
+.project-card {
+  display: -webkit-inline-box;
+  display: -ms-inline-flexbox;
+  display: inline-flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  background-color: #ffffff;
+  border-radius: 3px;
+  -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12),
+    0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  width: 300px;
+  margin: 15px;
+}
+
+.project-card > .heading {
+  height: 130px;
+  padding: 10px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-box-flex: 0;
+  -ms-flex: 0 0 auto;
+  flex: 0 0 auto;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  background-color:  #213377;
+  color: white;
+}
+.project-card > .heading > h6 {
+  text-align: center;
+  font-size: 1.3em;
+  margin: 0;
+}
+.project-card > .body {
+  min-height: 60px;
+  padding: 20px 10px;
+  -webkit-box-flex: 1;
+  -ms-flex: 1;
+  flex: 1;
+}
+.project-card > .body > p {
+  margin: 0;
+  font-size: 14px;
+}
+.project-card > .footer {
+  background-color: #f4f4f4;
+  border-top: 1px solid #cccccc;
+  padding: 10px;
+  font-size: 12px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-flex: 0;
+  -ms-flex: 0 0 auto;
+  flex: 0 0 auto;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-pack: justify;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
+.project-card > .footer > div {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
+.project-card > .footer a {
+  color: rgba(0, 0, 0, 0.8);
+  text-decoration: none;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+}
+.project-card > .footer i {
+  font-size: 15px;
+  margin-right: 8px;
+}
+.project-card > .footer .framework {
+  margin: 0;
+}
+
+.skills {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  width: 100%;
+  max-width: 1140px;
+}
+.skills .skills-section {
+  -webkit-box-flex: 1;
+  -ms-flex: 1 0 auto;
+  flex: 1 0 auto;
+  text-align: center;
+}
+
+ul {
+  padding: 0;
+}
+ul li {
+  list-style-type: none;
+}
+
+.social-links {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
+.social-links i,
+.social-links svg {
+  margin: 0 15px;
+  color: #dfe6e9;
+  fill: #dfe6e9;
+  font-size: 24px;
+  height: 24px;
+  width: 24px;
+}
+
+.copyright {
+  text-align: center;
+}
+
+@media (max-width: 1140px) {
+  .center-projects {
+    width: 100%;
+  }
+}
+
+@media (max-width: 540px) {
+  .hero .ml1 {
+    font-size: 3em;
+  }
+  .hero .ml16 {
+    font-size: 1.5em;
+  }
+  .section {
+    padding: 15px;
+  }
+  .subheading {
+    font-size: 1.2em;
+  }
+  .skills {
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+  }
+  .skills .skills-section {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 0 100%;
+    flex: 1 0 100%;
+  }
+}
+@media (max-width: 420px) {
+  .hero .ml1 {
+    font-size: 2.5em;
+  }
+  .hero .ml16 {
+    font-size: 1em;
+  }
+}
+@media (max-width: 350px) {
+  .hero .ml1 {
+    font-size: 2.2em;
+  }
+}
+
+</style>
