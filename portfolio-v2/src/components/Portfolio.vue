7fc52e7ae7c93cb0b582d8e81c9cc270082954da
Bill Kariri <billkariri@gmail.com>
2020-03-03 20:46:18 +0300

    add some shared styles to the portfolio component

--- portfolio-v2/src/components/Portfolio.vue
+++ portfolio-v2/src/components/Portfolio.vue
@@ -21,5 +21,28 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-
+.section {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-box-pack: start;
+  -ms-flex-pack: start;
+  justify-content: flex-start;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  padding: 40px;
+}
+.section.light {
+  background-color: #dfe6e9;
+  color: rgba(0, 0, 0, 0.8);
+}
+.section.dark {
+  background-color: #213377;
+  color: #dfe6e9;
+}
 </style>
